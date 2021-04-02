3a5b88ed21b3ad61835712cc76b217eb73f02d50
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-04-02 19:27:48 -0300

    [feat] NewTransactionModal: data base, data in API

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createServer} from 'miragejs';
+import {createServer, Model} from 'miragejs';
 import {App} from './App';
 
 createServer({
+
+  //para conectar as duas rotas (get e post)
+  //usar o banco de dados interno do Miragejs
+  //schema é o banco de dados
+
+  models: {
+    transaction: Model,
+  },
   routes() {
 
     this.namespace = 'api';
 
-    this.get('transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transacion 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'comida',
-          createdAt: new Date(),
-        }
-      ]
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+
+      const data =  JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
