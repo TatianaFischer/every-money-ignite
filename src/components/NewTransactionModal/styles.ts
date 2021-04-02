5ab0a6aad49fb671283babaa76cabda91ff912f4
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-04-02 18:28:09 -0300

    [style] NewTransactionModal: button colors with styled component props + polished transparentize

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import {darken} from 'polished'
+import {darken, transparentize} from 'polished'
 
 export const Container = styled.form`
  h2 {
@@ -63,6 +63,14 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
    isActive: boolean;
+   activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E62E4D',
+
+
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -71,7 +79,11 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? 'blue' : 'transparent'};
+  background: ${(props) => props.isActive
+    ?  transparentize(0.9, colors[props.activeColor])
+    : 'transparent'
+  
+  };
 
   display: flex;
   align-items: center;
